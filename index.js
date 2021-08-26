bff9883aec269b3ee1177feb65e7e15a52ce56f1
nishank1.soni <nishank.soni@rupeek.com>
2021-08-26 15:01:40 +0530

    Added proxy middleware code in index.js

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
-const express = require('express');
+const express = require("express");
 const morgan = require("morgan");
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
+const { createProxyMiddleware,fixRequestBody} = require("http-proxy-middleware");
+const _ = require('lodash');
 // Create Express Server
 const app = express();
-
+const URL =  "https://uat.rupeek.co";
 // Configuration
 const PORT = 3000;
 const HOST = "localhost";
-const API_SERVICE_URL = "https://jsonplaceholder.typicode.com";
+// Logging : This tells express to log via morgan and morgan to log in the "combined" pre-defined format // That's it. Everything in your snippet after this are just other variations your might want to use
+app.use(morgan('combined'))
+app.use(express.json());
 
-// Logging
-app.use(morgan('dev'));
 
-// Info GET endpoint
-app.get('/info', (req, res, next) => {
-    res.send('This is a proxy service which proxies to JSONPlaceholder API.');
-});
+var validatingResponse = function(proxyRes, req, res) 
+{
+    const exchange = `[${req.method}] [${proxyRes.statusCode}] ${req.path} -> ${proxyRes.req.protocol}//${proxyRes.req.host}${proxyRes.req.path}`;
+    console.log(exchange); // [GET] [200] / -> http://www.example.com
+};
 
-// Authorization
-app.use('', (req, res, next) => {
-    if (req.headers.authorization) {
-        next();
-    } else {
-        res.sendStatus(403);
+var restream = function(proxyReq, req, res, options) {
+    if (req.body) 
+    {
+        const newLoanRequestObj = _.omit(req.body, [
+            "pledgecards",
+            "coBorrowerDetails",
+            "phone"
+        ]);
+        let bodyData = JSON.stringify(newLoanRequestObj);
+        proxyReq.setHeader('Content-Type','application/json');
+        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        // stream the content
+        proxyReq.write(bodyData);
     }
+}
+
+
+var apiProxy = createProxyMiddleware('/testing', {
+    target: URL,
+    pathRewrite: {[`^/testing`]: "",},
+    changeOrigin: true,// to handle mismatching SSL certificate 
+    onProxyReq: restream,
+    onProxyRes: validatingResponse
 });
 
-// Proxy endpoints
-app.use('/json_placeholder', createProxyMiddleware({
-    target: API_SERVICE_URL,
-    changeOrigin: true,
-    pathRewrite: {
-        [`^/json_placeholder`]: '',
-    },
-}));
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(apiProxy);  
 
 // Start Proxy
 app.listen(PORT, HOST, () => {
